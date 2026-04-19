add472fe4333b204763680d26bf760f898ff1af3
adamprojectv2 <adammarvelprasetyo45@gmail.com>
2026-04-19 19:00:37 +0700

    XID

--- src/components/BoxOldWeb.jsx
+++ src/components/BoxOldWeb.jsx
@@ -30,7 +30,7 @@ const BoxOldWeb = () => {
 			{/* CARD */}
 			<div onClick={handleOpen} className="cursor-pointer">
 				<div className="flex justify-between">
-					<img src="/link.png" alt="" className="w-auto h-6" />
+					<img src="/avatar.png" alt="" className="w-auto h-6" />
 					<img src="/next.png" alt="" className="h-3 w-3" />
 				</div>
 				<h1 className="text-white text-base font-semibold pr-0 mt-5">
