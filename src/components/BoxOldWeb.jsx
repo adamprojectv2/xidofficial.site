c85989d9a1b3208eaf91d1e60ef44d50f66a41d0
adamprojectv2 <adammarvelprasetyo45@gmail.com>
2026-05-02 21:03:41 +0700

    XID

--- src/components/BoxOldWeb.jsx
+++ src/components/BoxOldWeb.jsx
@@ -7,7 +7,7 @@ const BoxOldWeb = () => {
 					<img src="/avatar.png" alt="" className="w-auto h-6" />
 					<img src="/next.png" alt="" className="h-3 w-3" />
 				</div>
-				<h1 className="text-white text-base font-semibold pr-0 mt-5">Daftar Anggota</h1>
+				<h1 className="text-white text-base font-semibold pr-0 mt-5">Menu Lainnya</h1>
 			</a>
 		</div>
 	)
